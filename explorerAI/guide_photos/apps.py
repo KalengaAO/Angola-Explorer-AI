386831b1e95afd6ca8c181b62a5a5a9b6b31a967
Antonio Pedro Kalenga <akalenga@c3r3s3.42luanda.com>
2026-07-19 00:14:14 +0100

    all app

--- explorerAI/guide_photos/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GuidePhotosConfig(AppConfig):
-    name = 'guide_photos'
--- explorerAI/guide_specialties/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GuideSpecialtiesConfig(AppConfig):
-    name = 'guide_specialties'
